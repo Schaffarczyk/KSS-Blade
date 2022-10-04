d6cdd298bfa5dd8f26a91b7b60e4d37eb35d92c5
Schaffarczyk <55233361+Schaffarczyk@users.noreply.github.com>
2022-10-04 17:01:11 +0200

    Delete Source-code directory

--- Source-code/compDesCode.cmd
+++
@@ -1,6 +0,0 @@
-echo 'compile module mem'
-gfortran -c  mem.f
-echo 'compile rest'
-gfortran -c  -O3  -fno-automatic -fbounds-check KSS.f  Sub.f SubNum.f
-echo 'link all together'
-gfortran mem.o KSS.o  Sub.o SubNum.f -o KSS.exe
