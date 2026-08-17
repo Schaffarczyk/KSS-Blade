7e3038c01905e5435a0c759f5db98bba4ddac95b
Bhima Masare <bhimamasare@gmail.com>
2026-08-17 19:40:45 +0200

    Commit_00 - The system files were deleted on 17.08.2026 in Branch - V5

--- SourceCode/compDesCode.cmd
+++
@@ -1,6 +0,0 @@
-echo 'compile module mem'
-gfortran -c  mem.f
-echo 'compile rest'
-gfortran -c  -O3  -fno-automatic -fbounds-check KSS.f  Sub1.f  Sub2.f SubNum.f
-echo 'link all together'
-gfortran mem.o KSS.o  Sub1.o Sub2.o SubNum.f -o KSS.exe
